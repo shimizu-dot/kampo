16656d50cd1a868aac4269c01c472609bb55854c
Hiromi SHIMIZU <202601_aw_shimizu@x-ide.sakura.ne.jp>
2026-06-17 15:54:19 +0900

    First commit

---
+++ src/main/java/com/example/kanpo/KanpoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kanpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanpoApplication.class, args);
+	}
+
+}
